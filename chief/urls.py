7bee88ed2e9732e4bbb073f52dc3d4fb10c5e8e9
michael54 <yuyu666306@gmail.com>
2012-10-07 00:24:00 +0000

    Basic Framework

--- chief/urls.py
+++ chief/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
