5d53bc9f041d95a9811a69f14a1a7933e51f51fd
michael54 <yuyu666306@gmail.com>
2012-10-08 21:43:30 +0000

    Basic food model finished

--- food/admin.py
+++ food/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from food.models import Food
+from food.models import FoodCategory
 
 admin.site.register(Food)
-
+admin.site.register(FoodCategory)
--- food/models.py
+++ food/models.py
@@ -1,19 +1,37 @@
 from django.db import models
+import os
+import uuid
 
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('food_photos', filename)
 
 # Create your models here.
 
+class FoodCategory(models.Model):
+	"""Food Category Model"""
+	name = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.name
+
 
 class Food(models.Model):
-	"""docstring for Food"""
+	"""Food Model, managing all kinds of food"""
 	
-	name = models.CharField(max_length=50)
+	name = models.CharField(max_length=100)
+	category = models.ForeignKey(FoodCategory)
 	brief = models.CharField(max_length=1000)
-	storage_time = models.CharField(max_length=50)
-	storage_method = models.CharField(max_length=1000)
-	recipe_num = models.IntegerField()
-	like_num = models.IntegerField()
-	recipes = models.ManyToManyField('recipe.Recipe', verbose_name="list of recipes")
+	cover_image = models.ImageField(upload_to=get_file_path, null=True, verbose_name=u'Cover image');
+	storage_time = models.CharField(max_length=50, blank=True)
+	storage_method = models.CharField(max_length=1000, blank=True)
+	recipe_num = models.IntegerField(default=0)
+	like_num = models.IntegerField(default=0)
+	recipes = models.ManyToManyField('recipe.Recipe', null=True, verbose_name=u'list of recipes')
+	pick_method = models.CharField(max_length=1000, blank=True, verbose_name=u'How to choose')
+	food_efficacy = models.CharField(max_length=1000, blank=True)
+
 
 	def __unicode__(self):
 		return self.name
