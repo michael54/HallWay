33165aa8133aead0b9790523fd44b872492b7110
michael54 <yuyu666306@gmail.com>
2012-10-21 00:08:30 +0000

    Add comments app, change profile

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from userena.models import UserenaLanguageBaseProfile
+from recipe.models import Recipe
 
 class MyProfile(UserenaLanguageBaseProfile):
     user = models.OneToOneField(User,
                                 unique=True,
                                 verbose_name=_('user'),
                                 related_name='my_profile')
-    favourite_snack = models.CharField(_('favourite snack'),
-                                       max_length=5)
+
+    favourite_recipes = models.ManyToManyField(Recipe,
+    							related_name='favourite_recipes')
+
+
--- chief/settings.py
+++ chief/settings.py
@@ -129,7 +129,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    
+    'django.contrib.comments',
+        
     # Plugins
     'django_evolution',
     'userena',
