22203cb5b7eeba23977b97cfbf168fa4c3c285ae
HarveyZhang <zaluy.z@gmail.com>
2012-11-01 12:09:51 +0000

    modify navbar and try opacity of background

---
+++ static/js/recipe/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	window.onload = function () {
+		$('body').show();
+		$('body').animate({
+			//opacity: 0.5,
+		}, 2000, function() {
+			// change img
+		});
+	};
+});
