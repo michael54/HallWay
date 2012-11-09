2ba1caf60daff3b0971bd5cf2998aac5aeda9318
michael54 <yuyu666306@gmail.com>
2012-11-09 06:51:08 +0000

    Project: enable email sending to users through a gmail account

--- chief/settings.py
+++ chief/settings.py
@@ -195,7 +195,13 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'cmpt470ido@gmail.com'
+EMAIL_HOST_PASSWORD = 'tebuhaoweizhangyuyu'
 
 
 ANONYMOUS_USER_ID = -1
