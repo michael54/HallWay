0e0da7954361c3fb3f471cf3963a5f9f652817fc
HarveyZhang <zaluy.z@gmail.com>
2012-11-23 22:58:47 +0000

    modification of recipe_detail food_detail

--- static/js/food/food_detail.js
+++ static/js/food/food_detail.js
@@ -1,23 +1,22 @@
-var j = jQuery.noConflict();
-j(document).ready(function () {
+jQuery(document).ready(function () {
 	// hide string content in brief content of the part more than length = 50
-	var $bContent = j('div.brief-intro').text();
+	var $bContent = jQuery('div.brief-intro div').text();
 	var bLength = $bContent.length;
 	if (bLength > 70) {
 		// $clicedRemaining = $bContent.slice(100);
 		$slicedContent = $bContent.slice(0, 100) + '...   ';
-		j('div.brief-intro').text($slicedContent);
-		j('a.hide-show').show();
+		jQuery('div.brief-intro div').text($slicedContent);
+		jQuery('a.hide-show').show();
 		// alert($bContent);
 	}
-	j('a.hide-show').toggle(
+	jQuery('a.hide-show').toggle(
 		function () {
 			$showContent = $bContent;
-			j('div.brief-intro').text($showContent);
-			j('a.hide-show').text('Hide');
+			jQuery('div.brief-intro div').text($showContent);
+			jQuery('a.hide-show').text('Hide');
 		}, function() {
 			$slicedContent = $bContent.slice(0, 100) + '...   ';
-			j('div.brief-intro').html($slicedContent);
-			j('a.hide-show').text('Show more');
+			jQuery('div.brief-intro div').html($slicedContent);
+			jQuery('a.hide-show').text('Show more');
 	});
 });
