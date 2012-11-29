eb7c6817bf2703219b03c530263290624c5696cd
HarveyZhang <zaluy.z@gmail.com>
2012-11-29 08:56:00 +0000

    index page redo: unfinished

--- static/js/recipe/index.js
+++ static/js/recipe/index.js
@@ -11,9 +11,31 @@ jQuery(document).ready(function () {
     }
   );
 
-  // index page recipe category show
-  // var pLeft = jQuery('li#item3').position().left;
-  // console.log(pLeft);
+  var resizeDiv = function () {
+    dHeight = window.innerHeight;
+    jQuery('ul.upper-level > li').css('top', dHeight*0.2);
+    jQuery('a.show-control').css('top', dHeight*0.2+200);
+    // jQuery('ul.circular-show-inner.lower-level > li').css('top', dHeight*0.4);
+    jQuery('ul.lower-level').css('top', dHeight*0.2+150);
+    var dWidth = jQuery('li.show-item').width()*5;
+    var dLeft = (window.innerWidth- dWidth)/2;
+    jQuery('ul.lower-level > li').each(function (i) {
+      jQuery(this).css('left', 144*i);
+    });
+    jQuery('ul.lower-level').css('left', dLeft);
+    jQuery('a#pItem').css('left', dLeft-60);
+    jQuery('a#nItem').css('left', dLeft+dWidth+20);
+    jQuery('ul.upper-level > li#item1').css('left', dLeft);
+    jQuery('ul.upper-level > li#item2').css('left', dLeft+144);
+    jQuery('ul.upper-level > li#item3').css('left', dLeft+144*2);
+    jQuery('ul.upper-level > li#item4').css('left', dLeft+144*3);
+    jQuery('ul.upper-level > li#item5').css('left', dLeft+144*4);
+    jQuery('ul.lower-level').css('width', dWidth);
+    jQuery('ul.lower-level').css('height', jQuery('ul.lower-level > li >a').height());
+  }
+  resizeDiv();
+  jQuery(window).resize(resizeDiv);
+
   var move = 0;
   jQuery('li.show-item').toggle(function () {
     move = jQuery('li#item3').position().left - jQuery(this).position().left;
@@ -21,6 +43,7 @@ jQuery(document).ready(function () {
     jQuery('ul.circular-show-inner.upper-level > li').animate({
       'left': '+='+move
     }, {queue:false, duration: 1000, complete: function () {
+      jQuery('.lower-level').show();
       jQuery('ul.circular-show-inner.lower-level > li').show();
     }});
     jQuery(this).siblings().fadeOut(1000);
