a5bbde85934e7ad142b9e5b06d0e41988b078a5d
HarveyZhang <zaluy.z@gmail.com>
2012-12-03 19:25:51 +0000

    add scroll up & scroll down

--- static/js/zhEffect.js
+++ static/js/zhEffect.js
@@ -5,10 +5,21 @@ jQuery(document).ready(function () {
         } else {
             jQuery('a.scroll-up').fadeOut();
         }
+    });
+    jQuery(window).scroll(function(){
+        if (jQuery(this).scrollTop() + jQuery(this).height() == jQuery(document).height()) {
+            jQuery('a.scroll-down').fadeOut();
+        } else {
+            jQuery('a.scroll-down').fadeIn();
+        }
     }); 
 
     jQuery('a.scroll-up').click(function(){
         jQuery("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+    jQuery('a.scroll-down').click(function(){
+        jQuery("html, body").animate({ scrollTop: jQuery(document).height() }, 600);
         return false;
     });
 });
