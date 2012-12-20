ed0b08274d88889380c5c7002fca9fe73aecdca2
Michael <yuyu666306@gmail.com>
2012-12-19 20:32:38 -0800

    added django compressor

--- chief/settings.py
+++ chief/settings.py
@@ -84,6 +84,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -137,6 +138,7 @@ INSTALLED_APPS = (
     'django.contrib.comments',
         
     # Plugins
+    'compressor',
     'djcelery',
     # 'autofixture',
     'imagekit',
